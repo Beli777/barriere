6f6bff9e45469b4b96fb727fd546dcd30eccc670
Stefan Karan <stefan.karan@diwanee.com>
2017-02-28 15:55:24 +0100

    update font && and test Georgia

--- js/editing.js
+++ js/editing.js
@@ -15,58 +15,58 @@ $(document).ready(function(){
     var text        = $('.editing-persons__info-body');
     var personTitle = $('.editing-persons__person-title');
     
-      if ($(window).width() > 640) {
-        
-        // init controller
-        var controller = new ScrollMagic.Controller();
-        var tlPersons = new TimelineLite()
-        
-        //----- Animations
-        // Body
-        .to(body, .1, {
-          opacity: 1,
-          ease: Power0.easeNone
-        }, '0')
-        // First image
-        .from(img1, .8, {
-          x: '-100%',
-          opacity: 0,
-          ease: Power0.easeNone
-        }, '0')
-        // Second image && Top title
-        .from([img2, titleTop], .8, {
-          y: '-100%',
-          opacity: 0,
-          ease: Power0.easeNone
-        }, '0')
-        // Third image && paragraph text
-        .from([img3, text], .8, {
-          y: '100%',
-          opacity: 0,
-          ease: Power0.easeNone
-        }, '0')
-        // Fourth image && Bottom title
-        .from([img4, titleBottom], .8, {
-          x: '100%',
-          opacity: 0,
-          ease: Power0.easeNone
-        }, '0')
-        // Names of people
-        .to(personTitle, 1, {
-          opacity: 1
-        }, '0+=0.5')
-        
-        // build a scene
-        var scene = new ScrollMagic.Scene ({
-          triggerElement: el,
-          triggerHook: 0.5,
-          reverse: false
-        })
-        
-        .setTween(tlPersons) // trigger a TweenMax tween
-        .addTo(controller);
+    if ($(window).width() > 640) {
+      
+      // init controller
+      var controller = new ScrollMagic.Controller();
+      var tlPersons = new TimelineLite()
+      
+      //----- Animations
+      // Body
+      .to(body, .1, {
+        opacity: 1,
+        ease: Power0.easeNone
+      }, '0')
+      // First image
+      .from(img1, .8, {
+        x: '-100%',
+        opacity: 0,
+        ease: Power0.easeNone
+      }, '0')
+      // Second image && Top title
+      .from([img2, titleTop], .8, {
+        y: '-100%',
+        opacity: 0,
+        ease: Power0.easeNone
+      }, '0')
+      // Third image && paragraph text
+      .from([img3, text], .8, {
+        y: '100%',
+        opacity: 0,
+        ease: Power0.easeNone
+      }, '0')
+      // Fourth image && Bottom title
+      .from([img4, titleBottom], .8, {
+        x: '100%',
+        opacity: 0,
+        ease: Power0.easeNone
+      }, '0')
+      // Names of people
+      .to(personTitle, 1, {
+        opacity: 1
+      }, '0+=0.5')
+      
+      // build a scene
+      var scene = new ScrollMagic.Scene ({
+        triggerElement: el,
+        triggerHook: 0.5,
+        reverse: false
+      })
+      
+      .setTween(tlPersons) // trigger a TweenMax tween
+      .addTo(controller);
 
-      }
+    }
     
   }
   
@@ -86,109 +86,87 @@ $(document).ready(function(){
     var title           = $('.editing-sport .editing__title');
     var info            = $('.editing-sport .editing__info-body');
     
-    // el.one('inview', function(event, isInView) {
-      if ($(window).width() > 640) {
-        
-        // init controller
-        var controller = new ScrollMagic.Controller();
-        var tlSport = new TimelineLite()
-        
-        //----- Animations
-        // Body
-        tlSport.to(body, .1, {
-          opacity: 1,
-          ease: Power0.easeNone
-        }, '0')
-        // Golf image 1
-        tlSport.from(imgGolf1, .5, {
-          x: '-100%',
-          y: '-100%',
-          opacity: .5,
-          ease: Power0.easeNone
-        }, '0')
-        // Golf image 2
-        tlSport.from(imgGolf2, .5, {
-          x: '100%',
-          y: '-100%',
-          opacity: 0,
-          // delay: .2,
-          ease: Power0.easeNone
-        }, '0+=0.2')
-        // Point Left
-        tlSport.from(pointLeft, .3, {
-          x: '-300%',
-          opacity: 0,
-          // delay: .5,
-          ease: Power0.easeNone
-        }, '0+=0.5')
-        // Point Right
-        tlSport.from(pointRight, .3, {
-          x: '300%',
-          opacity: 0,
-          // delay: .5,
-          ease: Power0.easeNone
-        }, '0+=0.5')
-        // Num
-        tlSport.from(num, .5, {
-          opacity: 0,
-          // delay: .5,
-          ease: Power0.easeNone
-        }, '0+=0.5')
-        // Info & title
-        tlSport.from([title, info], .3, {
-          y: '100%',
-          opacity: 0,
-          // delay: .5,
-          ease: Power0.easeNone
-        }, '0+=0.5')
-        // Tennis racket 1
-        tlSport.from(imgTennis1, .5, {
-          position: 'relative',
-          left: '-12%',
-          top: -150,
-          rotation: -90,
-          opacity: 0,
-          ease: Power0.easeNone
-        }, '0')
-        // Tennis racket 2
-        tlSport.from(imgTennis2, .5, {
-          position: 'relative',
-          left: '5%',
-          top: 100,
-          rotation: -150,
-          opacity: 0,
-          ease: Power0.easeNone
-        }, '0')
+    if ($(window).width() > 640) {
       
-        // build a scene
-        var scene = new ScrollMagic.Scene ({
-          triggerElement: el,
-          triggerHook: 0.7,
-          reverse: false
-        })
-        
-        .setTween(tlSport) // trigger a TweenMax tween
-        .addTo(controller);
+      // init controller
+      var controller = new ScrollMagic.Controller();
+      var tlSport = new TimelineLite()
       
-      }
-      
-    // });
+      //----- Animations
+      // Body
+      tlSport.to(body, .1, {
+        opacity: 1,
+        ease: Power0.easeNone
+      }, '0')
+      // Golf image 1
+      tlSport.from(imgGolf1, .5, {
+        x: '-100%',
+        y: '-100%',
+        opacity: .5,
+        ease: Power0.easeNone
+      }, '0')
+      // Golf image 2
+      tlSport.from(imgGolf2, .5, {
+        x: '100%',
+        y: '-100%',
+        opacity: 0,
+        ease: Power0.easeNone
+      }, '0+=0.2')
+      // Point Left
+      tlSport.from(pointLeft, .3, {
+        x: '-300%',
+        opacity: 0,
+        ease: Power0.easeNone
+      }, '0+=0.5')
+      // Point Right
+      tlSport.from(pointRight, .3, {
+        x: '300%',
+        opacity: 0,
+        ease: Power0.easeNone
+      }, '0+=0.5')
+      // Num
+      tlSport.from(num, .5, {
+        opacity: 0,
+        ease: Power0.easeNone
+      }, '0+=0.5')
+      // Info & title
+      tlSport.from([title, info], .3, {
+        y: '100%',
+        opacity: 0,
+        ease: Power0.easeNone
+      }, '0+=0.5')
+      // Tennis racket 1
+      tlSport.from(imgTennis1, .5, {
+        position: 'relative',
+        left: '-12%',
+        top: -150,
+        rotation: -90,
+        opacity: 0,
+        ease: Power0.easeNone
+      }, '0')
+      // Tennis racket 2
+      tlSport.from(imgTennis2, .5, {
+        position: 'relative',
+        left: '5%',
+        top: 100,
+        rotation: -150,
+        opacity: 0,
+        ease: Power0.easeNone
+      }, '0')
     
+      // build a scene
+      var scene = new ScrollMagic.Scene ({
+        triggerElement: el,
+        triggerHook: 0.7,
+        reverse: false
+      })
+      
+      .setTween(tlSport) // trigger a TweenMax tween
+      .addTo(controller);
+    
+    }
+      
   }
-  
-  //----- Paragraph elements / adding class to .editing__p which are in viewport 
-  // OLD VERSION !!!
-  // function fadeText() {
-  //   $('.editing__p').each(function() {
-  //     $(this).on('inview', function(event, isInView) {
-  //       if (isInView && $(window).width() > 640) {
-  //         $(this).addClass('InView');
-  //       } else {
-  //         $(this).removeClass('InView');
-  //       }
-  //     });
-  //   });
-  // }
   
   //----- Fade text
   // New Version
@@ -249,9 +227,6 @@ $(document).ready(function(){
         
         .setTween(tween) // trigger a TweenMax tween
         .addTo(controller);
-        
-        // run animation once
-        // set new reverse option
   
       });
       
@@ -272,9 +247,6 @@ $(document).ready(function(){
         
         .setTween(tween) // trigger a TweenMax tween
         .addTo(controller);
-        
-        // run animation once
-        // set new reverse option
   
       });
     }
